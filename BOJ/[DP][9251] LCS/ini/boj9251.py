39e15087e7b3682b634906073077c4ff81315e49
yonini <hardworkingini@gmail.com>
2023-02-07 01:29:07 +0900

    [DP][9251] LCS

--- BOJ/[DP][9251] LCS/ini/boj9251.py
+++ BOJ/[DP][9251] LCS/ini/boj9251.py
@@ -1,23 +1,14 @@
 import sys
 arr1 = list(sys.stdin.readline())
 arr2 = list(sys.stdin.readline())
-n, m = len(arr1) - 1, len(arr2) - 1
-dp = [0 for _ in range(n)]
+n, m = len(arr1), len(arr2)
+dp = [[0 for _ in range(m)] for _ in range(n)]
 
-for i in range(n):
-    k, j = i, 0
-    while(j < m):
-        if(k == n):
-           break
+for i in range(1, n):
+    for j in range(1, m):
+        if(arr1[i - 1] == arr2[j - 1]):
+            dp[i][j] = dp[i - 1][j]  + 1
         else:
-            if(arr1[k] == arr2[j]):
-                dp[i] += 1
-                k += 1
-                t = j
-            else:
-                if(j == m - 1):
-                    j = t
-                    k += 1
-        j += 1
+            dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
 
-print(max(dp))
+print(max(dp[n - 1]))
