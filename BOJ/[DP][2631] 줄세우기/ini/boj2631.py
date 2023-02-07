91e782240d90caf4a55f3a1122101b797d51437f
yonini <hardworkingini@gmail.com>
2023-02-07 14:04:00 +0900

    [DP][2631] 줄 세우기

--- BOJ/[DP][2631] 줄세우기/ini/boj2631.py
+++ BOJ/[DP][2631] 줄세우기/ini/boj2631.py
@@ -1,3 +1,11 @@
 import sys
 N = int(sys.stdin.readline())
 arr = [int(sys.stdin.readline()) for _ in range(N)]
+dp = [1 for _ in range(N)]
+
+for i in range(N):
+    for j in range(i + 1, N):
+        if(arr[i] < arr[j]):
+            dp[j] = max(dp[i] + 1, dp[j])
+
+print(N - max(dp))
