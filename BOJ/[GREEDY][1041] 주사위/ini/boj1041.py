ffb3e65653bb084b96e12ad2b1f9dfcd085323be
yonini <hardworkingini@gmail.com>
2023-02-14 17:31:55 +0900

    코드 수정!

--- BOJ/[GREEDY][1041] 주사위/ini/boj1041.py
+++ BOJ/[GREEDY][1041] 주사위/ini/boj1041.py
@@ -1,3 +1,17 @@
 import sys
 N = int(sys.stdin.readline())
 arr = [int(x) for x in sys.stdin.readline().split()]
+min_arr = [min(arr[0], arr[5]), min(arr[1], arr[4]), min(arr[2], arr[3])]
+min_arr.sort()
+arr.sort()
+ans = 0
+
+if(N == 1):
+    ans = sum(arr[:5])
+
+else:
+    ans = 4 * sum(min_arr[:3])
+    ans += (4 * (N - 2) + 4 * (N - 1)) * sum(min_arr[:2])
+    ans += ((N - 2) * (N - 2) + 4 * (N - 2) * (N - 1)) * min_arr[0]
+
+print(ans)
--- BOJ/[GREEDY][11000] 강의실 배정/ini/boj11000.py
+++ BOJ/[GREEDY][11000] 강의실 배정/ini/boj11000.py
@@ -1,4 +1,10 @@
 import sys
 N = int(sys.stdin.readline().split())
-arr = [[int(x) for x in sys.stdin.readline().split()] for _ in range(N)]
+arr = [[int(x) for x in sys.stdin.readline().split()] for _ in range(N[0])]
+arr.sort()
+
+for i in range(N[0]):
+    if(arr[i] > 0):
+        B = i # 처음으로 양수되는 구간
+        break
 
