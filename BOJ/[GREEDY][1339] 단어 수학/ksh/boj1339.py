7f0b4efeb6e04faed73e4697ea48a949def6b49f
kwonssshyeon <kwonsh4396@gmail.com>
2023-02-21 15:28:54 +0900

    1715미완

--- BOJ/[GREEDY][1339] 단어 수학/ksh/boj1339.py
+++ BOJ/[GREEDY][1339] 단어 수학/ksh/boj1339.py
@@ -1,1 +1,8 @@
 import sys
+n = int(sys.stdin.readline())
+word = []
+for i in range(n):
+    word.append(list(sys.stdin.readline().strip('\n')))
+
+
+print(len(word[0]))
--- BOJ/[GREEDY][1715] 카드 정렬하기/ksh/boj1715.py
+++ BOJ/[GREEDY][1715] 카드 정렬하기/ksh/boj1715.py
@@ -1,1 +1,41 @@
 import sys
+n = int(sys.stdin.readline())
+num_list = []
+for i in range(n):
+    num_list.append(int(sys.stdin.readline()))
+num_list.sort()
+
+queue=[]
+sum = 0
+
+if n==1:
+    print(0)
+
+else:
+    queue.append(num_list[0]+num_list[1])
+    
+    i = 2
+    while i+1<n:
+        if queue[0]+num_list[i] < num_list[i]+num_list[i+1]:
+            queue.append(queue[0]+num_list[i])
+            sum+=queue.pop(0)
+            i+=1
+        else:
+            queue.append(num_list[i]+num_list[i+1])
+            i+=2
+    #num_list에 원소가 1개로 남았을때
+    if i == n-1:
+        queue.append(queue[0]+num_list[i])
+        sum+=queue.pop(0)
+        
+    #큐에 남은 원소 처리
+    i = 0
+    while len(queue)>1:
+        sum+=queue[0]+queue[1]
+        queue.append(queue[0]+queue[1])
+        queue.pop(0)
+        queue.pop(0)
+    if len(queue)==1:
+        sum+=queue.pop(0)
+
+    print(sum)
