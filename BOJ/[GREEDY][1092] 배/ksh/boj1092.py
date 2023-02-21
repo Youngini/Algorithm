e1712f56ad1129e2fae6f5171497256b77e46951
kwonssshyeon <kwonsh4396@gmail.com>
2023-02-22 03:09:02 +0900

    5주차

--- BOJ/[GREEDY][1092] 배/ksh/boj1092.py
+++ BOJ/[GREEDY][1092] 배/ksh/boj1092.py
@@ -4,19 +4,24 @@ n = int(sys.stdin.readline())
 max_weight = list(map(int,sys.stdin.readline().split()))
 m = int(sys.stdin.readline())
 boxes = list(map(int, sys.stdin.readline().split()))
-boxes.sort()
-max_weight.sort()
+boxes.sort(reverse=True)
+max_weight.sort(reverse=True)
 
-cnt = []
-num = 0
+cnt = 0
 
-if max_weight[n-1]<boxes[m-1]:
+if max_weight[0]<boxes[0]:
     print(-1)
 else:
-    for i in range(n):  #크레인 수 만큼
-        for j in range(m/n):    
-            if max_weight[i] < boxes[m/n*i+j]:
-                cnt+=1
-          
-
-print(cnt)
+    while len(boxes)>0:
+        cnt+=1 
+        for i in range(n):
+            if len(boxes)==0:
+                break
+            if max_weight[i] < boxes[-1]:
+                continue
+            for j in range(len(boxes)):
+                if max_weight[i] >= boxes[j]:
+                    boxes.pop(j)
+                    break
+        
+    print(cnt)
--- BOJ/[GREEDY][1339] 단어 수학/ksh/boj1339.py
+++ BOJ/[GREEDY][1339] 단어 수학/ksh/boj1339.py
@@ -4,5 +4,21 @@ word = []
 for i in range(n):
     word.append(list(sys.stdin.readline().strip('\n')))
 
+dict = {}
+for i in range(n):
+    for j in range(len(word[i])):
+        val = 10 ** (len(word[i])-j-1)
+        if word[i][j] in dict:
+            dict[word[i][j]] += val
+        else:
+            dict[word[i][j]] = val
 
-print(len(word[0]))
+sorted_dict = sorted(dict.items(), key=lambda x: -x[1])
+
+ans=0
+num=9
+for i in range(len(sorted_dict)):
+    ans += sorted_dict[i][1]*num
+    num-=1
+
+print(ans)
