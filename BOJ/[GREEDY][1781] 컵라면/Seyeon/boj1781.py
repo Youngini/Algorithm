ed970f29a8cfe008501e10748c3c21b34239634f
seyeonPark <tpdusdl1001@gmail.com>
2023-03-12 17:27:46 +0100

    boj1926 그림 Update

--- BOJ/[GREEDY][1781] 컵라면/Seyeon/boj1781.py
+++ BOJ/[GREEDY][1781] 컵라면/Seyeon/boj1781.py
@@ -5,23 +5,23 @@ input = sys.stdin.readline
 n = int(input())
 
 heap = []
-
 array = []
+
 for i in range(n):
     array.append(list(map(int, input().split())))
-    array[i].reverse() #0열이 컵라면수 , 1열이 데드라인
     
-array.sort(key=lambda x: (x[0], -x[1]))
+array.sort(reverse = True)
 
 
 for i in range(n):
-    if len(heap) < array[i][1]:
-        heapq.heappush(heap,[(-1)*array[i][0],array[i][1]])
-    elif abs(heap[0][0]) < array[i][0]:
+    if len(heap) < array[i][0]:
+        heapq.heappush(heap,array[i])
+    elif heap[0][1] < array[i][1]:
         heapq.heappop(heap)
-        heapq.heappush(heap,[(-1)*array[i][0],array[i][1]])
+        heapq.heappush(heap,array[i])
 
 result = 0
-while len(heap)!=0:
-    result += heapq.heappop(heap)[0]
-print(abs(sum(heap)))
+while len(heap)!= 0:
+   result += heapq.heappop(heap)[1]
+   
+print(result)
