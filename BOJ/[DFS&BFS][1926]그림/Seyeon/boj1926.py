4a38e3fa1c01bd7a6815b31c3777cdcc910c3bdb
seyeonPark <tpdusdl1001@gmail.com>
2023-03-13 11:43:43 +0100

    boj1927 그림 완료

--- BOJ/[DFS&BFS][1926]그림/Seyeon/boj1926.py
+++ BOJ/[DFS&BFS][1926]그림/Seyeon/boj1926.py
@@ -1,11 +1,34 @@
 import sys
-input = sys.stdin.readline
-n, m = map(int, input().split())
+sys.setrecursionlimit(10**7)
+
+def dfs(x,y):
+    global temp
+    if x<=-1 or x>=n or y<=-1 or y>=m:
+        return False
+    if graph[x][y] == 1:
+        graph[x][y] = 0
+        dfs(x-1,y)
+        dfs(x,y-1)
+        dfs(x+1,y)
+        dfs(x,y+1)
+        temp += 1
+        return True
+    return False
+
+n,m = map(int, input().split())
 
 graph = []
-
 for i in range(n):
-    graph.append(list(map(int, input(),split())))
+    graph.append(list(map(int, input().split())))
 
-
-        
+result = 0    
+maximum = 0
+for i in range(n):
+    for j in range(m):
+        temp = 0
+        if dfs(i, j) == True:
+            result += 1
+            if maximum < temp:
+                maximum = temp
+print(result)
+print(maximum)
